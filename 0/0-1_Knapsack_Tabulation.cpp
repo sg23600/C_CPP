ee6f21ea743b450c0a3065a4b65707c544522728
Sujay Goswami <53886504+sg23600@users.noreply.github.com>
2021-11-27 16:53:38 +0530

    Create 0-1_Knapsack_Tabulation.cpp
    using dynamic programming

---
+++ 0/0-1_Knapsack_Tabulation.cpp
@@ -0,0 +1,26 @@
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int dp[n+1][W+1];
+        int sol = 0;
+        
+        // for(int i=0; i<=W; i++) dp[0][i]=0;
+       for(int i=0; i<=n; i++)
+       {
+           for(int j=0; j<=W; j++)
+           {
+               if(i==0 or j==0) 
+                    dp[i][j] = 0;
+               
+               else if(j >= wt[i-1])
+                    dp[i][j] = max(
+                        dp[i-1][j],
+                        (dp[i-1][j-wt[i-1]]+val[i-1])
+                    );
+               
+               else 
+                    dp[i][j] = dp[i-1][j];
+           }
+       }
+    
+        return dp[n][W];
+    }
