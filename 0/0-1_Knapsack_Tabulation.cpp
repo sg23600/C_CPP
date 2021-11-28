1965b8ab0804303431edc8daff2008daf4649611
Sujay Goswami <53886504+sg23600@users.noreply.github.com>
2021-11-28 10:27:21 +0530

    Added recursion function
    Memorization

--- 0/0-1_Knapsack_Tabulation.cpp
+++ 0/0-1_Knapsack_Tabulation.cpp
@@ -1,3 +1,4 @@
+// Tabulation in for loop... O(N*W)
 int knapSack(int W, int wt[], int val[], int n) 
     { 
         int dp[n+1][W+1];
@@ -23,4 +24,22 @@ int knapSack(int W, int wt[], int val[], int n)
        }
     
         return dp[n][W];
+    }
+
+// Recursion using memorization... O(N*W)
+int dp[n][W]; 
+memset(dp, -1, sizeof(dp));
+
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        if(n==0 or W==0) return 0;
+        
+        if(dp[n][W]!=-1) return dp[n][W];
+        
+        if(wt[n-1] > W) return dp[n][W] = knapSack(W, wt, val, n-1);
+        else 
+            return dp[n][W] = max(
+            knapSack(W, wt, val, n-1),
+            val[n-1] + knapSack(W-wt[n-1], wt, val, n-1)
+            );
     }
